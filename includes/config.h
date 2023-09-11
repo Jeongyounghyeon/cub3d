095a63ef8eba0ad3f2afc9754996c2bcafef999a
Jeongyounghyeon <snorlax1106@naver.com>
2023-09-11 14:30:41 +0900

    add raycasting struct

--- includes/config.h
+++ includes/config.h
@@ -6,7 +6,7 @@
 /*   By: youjeong <youjeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 16:11:24 by youjeong          #+#    #+#             */
-/*   Updated: 2023/09/05 17:01:24 by youjeong         ###   ########.fr       */
+/*   Updated: 2023/09/11 14:30:08 by youjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define CONFIG_H
 
 # include "map.h"
+# include "ft_math.h"
 
 typedef struct s_config	t_config;
 typedef struct s_win	t_win;
@@ -22,14 +23,17 @@ typedef struct s_player	t_player;
 
 /* game data */
 typedef struct s_config {
-	t_win	*window;
-	t_img	*img;
+	t_win			*window;
+	t_img			*img;
+	t_raycasting	*raycasting;
 }t_config;
 
 /* mlx 구조체 */
 typedef struct s_win {
 	void	*mlx;
 	void	*win;
+	int		height;
+	int		width;
 }t_win;
 
 /* images */
@@ -42,11 +46,10 @@ typedef struct s_img {
 	void	*east;
 }t_img;
 
-typedef struct s_player
-{
-	double	x;
-	double	y;
-	double	direction[2];
-}	t_player;
+typedef struct s_raycasting {
+	t_vector	position;
+	t_vector	direction;
+	t_vector	screen;
+}t_raycasting;
 
 #endif
--- includes/ft_math.h
+++ includes/ft_math.h
@@ -1,39 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map.h                                              :+:      :+:    :+:   */
+/*   ft_math.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: youjeong <youjeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/04 16:59:25 by jy_23             #+#    #+#             */
-/*   Updated: 2023/09/11 13:58:09 by youjeong         ###   ########.fr       */
+/*   Created: 2023/09/11 13:44:19 by youjeong          #+#    #+#             */
+/*   Updated: 2023/09/11 13:46:01 by youjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAP_H
-# define MAP_H
+#ifndef FT_MATH_H
+# define FT_MATH_H
 
-typedef struct s_map	t_map;
+# include <math.h>
 
-typedef enum e_ele_id
-{
-	id_no,
-	id_so,
-	id_we,
-	id_ea,
-	id_f,
-	id_c
-}t_ele_id;
-
-typedef struct s_map
-{
-	char	**contents;
-	char	*elements[6];
-	int		height;
-	int		weight;
-	int		start_x;
-	int		start_y;
-	int		start_dir;
-}	t_map;
+typedef struct s_vector {
+	double	x;
+	double	y;
+}t_vector;
 
 #endif
