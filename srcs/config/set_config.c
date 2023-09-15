3a036d82eab12d91dedb3219872d696ea935cc7b
Jeongyounghyeon <snorlax1106@naver.com>
2023-09-15 12:11:07 +0900

    set screen vetor

--- srcs/config/set_config.c
+++ srcs/config/set_config.c
@@ -6,7 +6,7 @@
 /*   By: youjeong <youjeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:27:08 by youjeong          #+#    #+#             */
-/*   Updated: 2023/09/13 18:30:55 by youjeong         ###   ########.fr       */
+/*   Updated: 2023/09/15 12:10:36 by youjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	set_config(t_config *config, t_map *map)
 	config->map = map->contents;
 	config_img(config, map->elements);
 	config_player(&config->player, map);
-	config->rc.screen.x = 0;
-	config->rc.screen.y = FOV;
+	config->rc.screen.x = config->player.dir.y;
+	config->rc.screen.y = config->player.dir.x * (-FOV);
 }
