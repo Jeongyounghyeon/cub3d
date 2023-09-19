13fc397a8516bcb7bf726d56a9d104c2b7273783
JooYoung <jy_23@JooYoung.local>
2023-09-19 18:51:02 +0900

    for checking rotate code

--- srcs/event_handler.c
+++ srcs/event_handler.c
@@ -6,7 +6,7 @@
 /*   By: jy_23 <jy_23@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 14:25:10 by youjeong          #+#    #+#             */
-/*   Updated: 2023/09/19 18:40:13 by jy_23            ###   ########.fr       */
+/*   Updated: 2023/09/19 18:50:17 by jy_23            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 #include "config.h"
 
 int			event_handler(int key_code, t_config *config);
-static void	move_event(char **map, t_player *player, int new_dir_x, int new_dir_y);
+static void	vt_move_event(char **map, t_player *player, int new_dir_x, int new_dir_y);
+static void	hr_move_h_event(char **map, t_player *player, int new_dir_x, int new_dir_y);
 static void	rotate_event(t_vec *vector, double theta);
 
 int	event_handler(int key_code, t_config *config)
@@ -26,29 +27,29 @@ int	event_handler(int key_code, t_config *config)
 	map = config->map;
 	player = &config->player;
 	if (key_code == KEY_W)
-		move_event(map, player, 1, 1);
+		hr_move_h_event(map, player, 1, 1);
 	else if (key_code == KEY_A)
-		move_event(map, player, -1, 1);
+		vt_move_event(map, player, -1, 1);
 	else if (key_code == KEY_S)
-		move_event(map, player, -1, -1);
+		hr_move_h_event(map, player, -1, -1);
 	else if (key_code == KEY_D)
-		move_event(map, player, 1, -1);
+		vt_move_event(map, player, 1, -1);
 	else if (key_code == KEY_LEFT)
 	{
 		rotate_event(&config->player.dir, ROT_SPEED);
-		rotate_event(&config->rc.camera, ROT_SPEED);
+		//rotate_event(&config->rc.camera, ROT_SPEED);
 	}
 	else if (key_code == KEY_RIGHT)
 	{
 		rotate_event(&config->player.dir, -ROT_SPEED);
-		rotate_event(&config->rc.camera, -ROT_SPEED);
+		//rotate_event(&config->rc.camera, -ROT_SPEED);
 	}
 	else if (key_code == KEY_ESC)
 		exit(0);
 	return (0);
 }
 
-static void	move_event(char **map, t_player *player, int new_dir_x, int new_dir_y)
+static void	hr_move_h_event(char **map, t_player *player, int new_dir_x, int new_dir_y)
 {
 	t_coord		*pos;
 	int			dir_x;
@@ -61,6 +62,21 @@ static void	move_event(char **map, t_player *player, int new_dir_x, int new_dir_
 		pos->x += dir_x * MOVE_SPEED;
 	if (map[(int)(pos->y + dir_y * MOVE_SPEED)][(int)(pos->x)] != WALL)
 		pos->y += dir_y * MOVE_SPEED;
+}
+
+static void	vt_move_event(char **map, t_player *player, int new_dir_x, int new_dir_y)
+{
+	t_coord		*pos;
+	int			dir_x;
+	int			dir_y;
+
+	pos = &player->pos;
+	dir_x = player->dir.x * new_dir_x;
+	dir_y = player->dir.y * new_dir_y;
+	if (map[(int)pos->y][(int)(pos->x + dir_x * MOVE_SPEED)] != WALL)
+		pos->x += dir_y * MOVE_SPEED;
+	if (map[(int)(pos->y + dir_y * MOVE_SPEED)][(int)(pos->x)] != WALL)
+		pos->y += dir_x * MOVE_SPEED;
 }
 
 static void	rotate_event(t_vec *vector, double theta)
